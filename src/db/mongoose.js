8eb88ef49616efdedc7ad5d262751afce55956d3
LawsonUK <chrislawsonuk@hotmail.com>
2019-06-16 22:22:04 +0100

    added validator package added validation to user and task models

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 	useNewUrlParser: true,
@@ -7,15 +8,47 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const User = mongoose.model('User', {
 	name: {
-		type: String
+		type: String,
+		required: true,
+		trim: true
+	},
+	email: {
+		type: String,
+		required: true,
+		trim: true,
+		lowercase: true,
+		validate(value) {
+			if (!validator.isEmail(value)) {
+				throw new Error('Email is invalid')
+			}
+		}
+	},
+	password: {
+		type: String,
+		required: true,
+		trim: true,
+		minlength: 7,
+		validate(value) {
+			if (value.toLowerCase().includes('password')) {
+				throw new Error('Password cannot contain "password"')
+			}
+		}
 	},
 	age: {
-		type: Number
+		type: Number,
+		default: 0,
+		validate(value) {
+			if (value < 0) {
+				throw new Error('Age must be a positive number')
+			}
+		}
 	}
 })
 
 // const me = new User({
-// 	name: 'Christopher',
+// 	name: '   Christopher	',
+// 	email: 'MYEMAIL@LAWSON.IO	',
+// 	password: 'phone098!',
 // 	age: 34
 // })
 
@@ -29,16 +62,18 @@ const User = mongoose.model('User', {
 
 const Task = mongoose.model('Task', {
 	description: {
-		type: String
+		type: String,
+		required: true,
+		trim: true
 	},
 	completed: {
-		type: Boolean
+		type: Boolean,
+		default: false
 	}
 })
 
 const task = new Task({
-	description: "Cook Father's Day lunch",
-	completed: false
+	description: "Cook Father's Day lunch"
 })
 
 task.save()
