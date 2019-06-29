3c94b8ae2e34ab0d76631f5c3c51080b8ebd7adf
LawsonUK <chrislawsonuk@hotmail.com>
2019-06-29 22:00:59 +0100

    added logout routes

--- src/routers/user.js
+++ src/routers/user.js
@@ -22,11 +22,33 @@ router.post('/users/login', async (req, res) => {
 		)
 		const token = await user.generateAuthToken()
 		res.send({
-			user,
+			user: user.getPublicProfile(),
 			token
 		})
 	} catch (e) {
 		res.status(400).send()
+	}
+})
+
+router.post('/users/logout', auth, async (req, res) => {
+	try {
+		req.user.tokens = req.user.tokens.filter(token => {
+			return token.token !== req.token
+		})
+		await req.user.save()
+		res.send()
+	} catch (e) {
+		res.status(500).send()
+	}
+})
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+	try {
+		req.user.tokens = []
+		await req.user.save()
+		res.send()
+	} catch (e) {
+		res.status(500).send()
 	}
 })
 
